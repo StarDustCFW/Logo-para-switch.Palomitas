028d6547efda38def01767e85e5b6c51885bc782
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2021-02-26 23:23:20 +0000

    update

---
+++ make.cmd
@@ -0,0 +1,13 @@
+@echo off 
+set root="C:\Program Files (x86)\WinRAR\"
+cd PK
+set ver=1.6
+echo %ver%>V
+
+title compile pito.py
+PyInstaller pito.py -i icon.ico -y
+
+title build exe file
+%root%rar a ..\Logo-para-switch.Palomitas-v%ver%.rar * AAA dist -m5 -sfx..\SFX\My.sfx -z"..\SFX\xfs.conf" -s -k 
+cd ..
+cmd -k
