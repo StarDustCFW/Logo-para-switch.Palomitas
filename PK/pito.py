f7c38fd5f8f0aeb5fcf3d870fcb3fdfc0c042e30
Kronos2308 <Kronos2308@gmail.com>
2024-04-01 04:29:43 +0100

    ips reduction

--- PK/pito.py
+++ PK/pito.py
@@ -7,67 +7,73 @@ from PIL import Image
 from pathlib import Path
 from ips_util import Patch
 
-def exec_cmd(cmd):
-    return subprocess.check_output(cmd.split()).decode()
+import io
+import ips
+import argparse
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Invalid arguments 3")
-        sys.exit(1)
-
-#    exefs_data = Path(sys.argv[1])
-    
-
-    new_logo = Image.open(sys.argv[2])
-    new_logo = new_logo.convert("RGBA")
-
-    if len(sys.argv) > 3:
-        old_logo = Image.open(sys.argv[3])
-    else:
-        old_logo = Image.open("logo.png")
-
-    old_logo = old_logo.convert("RGBA")
-    """
-    if not exefs_data.is_dir():
-        print("Invalid arguments 1")
-        sys.exit(1)
-
-    offset_file = Path(exefs_data,"offset")
-    build_id_file = Path(exefs_data,"build_id")
-    #Get The Data to build ips
-    with offset_file.open("r") as f:
-        offset = int(f.read())
-
-    with build_id_file.open("r") as f:
-        build_id = f.read()
-
-    with exefs_data.open("r") as f:
-        Datts = f.read()
-    """
-    Datts = sys.argv[1]
+def single(base_patch,Datts):
+    #Get Offset And build ID
     build_id = Datts[0:64]
     offset = int(Datts[64:-1]+Datts[-1])
 
     print (Datts)
     print (build_id)
     print (offset)
+    
+    tmp_p = ips.Patch()
+
+    for r in base_patch.records:
+        tmp_p.add_record(r.offset + offset, r.content, r.rle_size)
 
     #Build the patch
-    new_data =  (b'\x00' * offset) + new_logo.tobytes()
-    old_data =  (b'\x00' * offset) + old_logo.tobytes()
-    patch = Patch.create(old_data, new_data)
-	
     if len(sys.argv) > 4:
         patch_path = Path(sys.argv[4])
     else:
         patch_path = Path("ips", f"{build_id}.ips")
     #save the patch
     with patch_path.open("w+b") as f:
-        f.write(patch.encode())	
+        f.write(bytes(tmp_p))	
+
+def Multiple(base_patch,Datts):
+    print(base_patch)
+    print(Datts)
+    
+    # Using readlines()
+    file1 = open(Datts, 'r')
+    Lines = file1.readlines()
+     
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        txt = line.strip()
+        x = txt.split(",")
+        print(x)
+        single(base_patch,x[1]+x[2])
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Invalid arguments 3")
+        sys.exit(1)
+
+    #load the new Image 
+    new_logo = Image.open(sys.argv[2])
+    new_logo = new_logo.convert("RGBA")
+
+    #load the base image
+    if len(sys.argv) > 3:
+        old_logo = Image.open(sys.argv[3])
+    else:
+        old_logo = Image.open("logo.png")
+
+    old_logo = old_logo.convert("RGBA")
+    
+
+    base_patch = ips.Patch.create(old_logo.tobytes(), new_logo.tobytes())
+    Datts = sys.argv[1]
+    if Datts == 'data.txt':
+        Multiple(base_patch,Datts)
+    else:
+        single(base_patch,Datts)
 	
-"""		
-    data_file = Path(exefs_dir,"data")
-    with data_file.open("w") as f:
-        f.write(build_id + "" + str(offset))
-"""	
-	
+
+
